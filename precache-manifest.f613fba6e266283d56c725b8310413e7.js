5a5e4ed0785e2cd19969baeb6b8a2ee2b94d18a5
krystiano13 <krystiano13@interklasa.pl>
2020-05-31 16:44:32 +0200

    Updates

---
+++ precache-manifest.f613fba6e266283d56c725b8310413e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ccae23f8e14714c5d1c93b7154c4482",
+    "url": "/YourNotes/home/index.html"
+  },
+  {
+    "revision": "e9da10ff848744593962",
+    "url": "/YourNotes/home/static/css/main.553b286a.chunk.css"
+  },
+  {
+    "revision": "60a373a387c37444bf50",
+    "url": "/YourNotes/home/static/js/2.1cb25d1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/YourNotes/home/static/js/2.1cb25d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9da10ff848744593962",
+    "url": "/YourNotes/home/static/js/main.a3965684.chunk.js"
+  },
+  {
+    "revision": "6e2f1761c115baf9c42a",
+    "url": "/YourNotes/home/static/js/runtime-main.44dcc33c.js"
+  }
+]);
